23a481fe48947cc0137c15a74b74a02e8d055924
catress <catress@umich.edu>
2022-03-31 02:24:51 -0400

    Rudimentary PUT and DELETE handlers (untested)

--- backend/app/views.py
+++ backend/app/views.py
@@ -72,5 +72,51 @@ def events(request):
         response['events'] = nearby_events
         return JsonResponse(response)
 
+    elif request.method == 'PUT':
+        json_data = json.loads(request.body)
+
+        query = 'UPDATE events SET'
+
+        if 'title' in json_data:
+            query += f' title = {json_data["title"]}'
+
+        if 'description' in json_data:
+            query += f' description = {json_data["description"]}'
+
+        if 'address' in json_data:
+            query += f' address = {json_data["address"]}'
+
+            g = geocoder.osm(address)
+            if not g.ok:
+                return HttpResponse(status=400)
+
+            lat = g.lat
+            lon = g.lng
+
+            query += f' lat = {lat}'
+            query += f' lon = {lon}'
+
+        if 'start_time' in json_data:
+            query += f' start_time = {datetime.fromtimestamp(int(json_data["start_time"]))}'
+
+        if 'end_time' in json_data:
+            query += f' end_time = {datetime.fromtimestamp(int(json_data["end_time"]))}'
+
+        event_id = json_data['event_id']
+        query += f' WHERE event_id = {event_id};'
+
+        cursor = connection.cursor()
+        cursor.execute(query)
+
+        return HttpResponse(status=201)
+    
+    elif request.method == 'DELETE':
+        json_data = json.load(request.body)
+
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM events WHERE event_id = %s;', (json_data['event_id']))
+
+        return HttpResponse(status=201)
+
     else:
         return HttpResponse(status=404)
